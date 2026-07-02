631f0dead709abdd5c80e943f99c4de6146d7aa4
sanjeevivp1 <sanjeevivp1@gmail.com>
2026-07-02 11:24:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,7 @@
-apt update && apt upgrade -y
-apt install git -y           
-pip3 install -U pip    
+#!/bin/bash
 
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT /PyroBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PyroBot
-fi
+echo "Installing dependencies..."
+pip3 install --no-cache-dir -r requirements.txt
 
-cd /PyroBot
-pip3 install -U -r requirements.txt --force-reinstall
-echo "Starting Bot....✨"
+echo "Starting Bot..."
 python3 bot.py
-
-
